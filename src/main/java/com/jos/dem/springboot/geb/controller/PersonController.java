1643028211f502249803a90bbc910f233cb143e5
josdem <joseluis.delacruz@gmail.com>
2018-03-31 12:01:45 -0600

    Listing persons

--- src/main/java/com/jos/dem/springboot/geb/controller/PersonController.java
+++ src/main/java/com/jos/dem/springboot/geb/controller/PersonController.java
@@ -2,6 +2,10 @@ package com.jos.dem.springboot.geb.controller;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+import com.jos.dem.springboot.geb.service.PersonService;
+
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,11 +16,15 @@ import org.slf4j.LoggerFactory;
 @RequestMapping("persons/**")
 public class PersonController {
 
+  @Autowired
+  private PersonService personService;
+
   Logger log = LoggerFactory.getLogger(this.getClass());
 
   @RequestMapping(method=GET)
-  public String persons(){
+  public String persons(final Model model){
     log.info("Listing persons");
+    model.addAttribute("persons", personService.getAll());
     return "list";
   }
 
